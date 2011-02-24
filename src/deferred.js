a13acca73d2f1a6487459d90608d88b8ec000892
jaubourg <j@ubourg.net>
2011-02-24 17:39:18 +0100

    jQuery 1.6 Deferred Enhancements Proposal
    - Deferred code refactored and deported into its own file (src/deferred.js):
    
      + New helpers:
    
        * jQuery.whenAny( p1, p2, ... ):
          is resolved if at least one of the given arguments is resolved,
          rejected otherwise
    
        * jQuery.whenNone( p1, p2, ... ):
          is resolved if none of the arguments are, rejected otherwise
    
      + New promise methods:
    
        * promise.always( fn1, fn2, ... ):
          callbacks are executed whether the deferred is resolved or rejected
    
        * promise.and( p1, p2, ... ):
          equivalent to jQuery.when( promise, p1, p2, ... )
    
        * promise.or( p1, p2, ... ):
          equivalent to jQuery.whenAny( promise, p1, p2, ... )
    
        * promise.chain( fn ):
          filters the resolve value and returns a new promise.
          For instance:
    
              var defer = jQuery.Deferred(),
                  filtered = defer.chain(function( a, b ) {
                      return a * b;
                  });
    
              filtered.done(function( res ) {
                  console.log( res );
              });
    
              defer.resolve( 2, 3 );
    
          will output "6" in the console.
    
          Can also be used to chain deferreds:
    
              jQuery.ajax( "service1" ).chain(function( response ) {
                  // The first request succeeded
                  return jQuery.ajax( "service2", {
                      data: response
                  });
              }).done(function() {
                  // The second request succeeded
              });
    
        * promise.invert():
          returns a new promise that is rejected when the original one is
          resolved and vice versa
    
    - deferred unit tests are now in test/unit/deferred.js
      (234 test total)
    
    - in src/core.js:
    
      + moved code around so that the ready deferred is created only when
        needed (allows deferred code to be out of core.js)
    
      + jQuery.readyPromise() added to have access to the ready promise
        Ex: jQuery.readyPromise().then(...)

---
+++ src/deferred.js
@@ -0,0 +1,241 @@
+(function( jQuery ) {
+
+// and & or methods factory
+function andOrFactory( any ) {
+	any = any ? "whenAny" : "when";
+	return function( promise ) {
+		return function() {
+			var args = [ promise ];
+			args.push.apply( args, arguments );
+			return jQuery[ any ].apply( jQuery, args );
+		};
+	};
+}
+
+// promise and invert methods factory
+function promiseInvertFactory( invert ) {
+	return function( promise ) {
+		var cache;
+		return function( object ) {
+			var i;
+			if ( !cache ) {
+				cache = {};
+				for( i in pMethods ) {
+					cache[ i ] = invert ?
+						( pMethods[ i ] && promise[ pMethods[ i ] ] ||
+							dFactories[ i ]( cache ) ) :
+						promise[ i ];
+				}
+			}
+			if ( !object ) {
+				object = cache;
+			} else {
+				for( i in cache ) {
+					object[ i ] = cache[ i ];
+				}
+			}
+			return object;
+		};
+	};
+}
+
+// Deferred methods factories
+var dFactories = {
+		always: function( promise ) {
+			return function() {
+				var args = sliceDeferred.call( arguments, 0 );
+				promise.done( args ).fail( args );
+				return this;
+			};
+		},
+		and: andOrFactory(),
+		chain: function( promise ) {
+			return function( fn ) {
+				return jQuery.Deferred(function( defer ) {
+					promise.done(function() {
+						jQuery.when( fn.apply( this, arguments ) )
+							.done( defer.resolve )
+							.fail( defer.reject );
+					}).fail( defer.reject );
+				}).promise();
+			};
+		},
+		invert: promiseInvertFactory( true ),
+		or: andOrFactory( true ),
+		promise: promiseInvertFactory(),
+		then: function( promise ) {
+			return function( doneCallbacks, failCallbacks ) {
+				promise.done( doneCallbacks ).fail( failCallbacks );
+				return this;
+			};
+		}
+	},
+	// Promise methods
+	pMethods = {
+		always: "always",
+		done: "fail",
+		fail: "done",
+		invert: "promise",
+		isRejected: "isResolved",
+		isResolved: "isRejected",
+		promise: "invert"
+	},
+	iDeferred,
+	sliceDeferred = [].slice;
+
+// Add methods with no invert
+for( iDeferred in dFactories ) {
+	pMethods[ iDeferred ] = pMethods[ iDeferred ] || false;
+}
+
+// jQuery.when, jQuery.whenAny and jQuery.whenNone factory
+function whenFactory( any, invert ) {
+	return function( object ) {
+			var length = arguments.length,
+				deferred = length <= 1 && object && jQuery.isFunction( object.promise ) ?
+					object :
+					jQuery.Deferred(),
+				promise = deferred.promise();
+
+		if ( invert ) {
+			promise = promise.invert();
+		}
+
+		if ( length > 1 ) {
+			var array = sliceDeferred.call( arguments, 0 ),
+				count = length,
+				iCallback = function( index ) {
+					return function( value ) {
+						array[ index ] = arguments.length > 1 ?
+								sliceDeferred.call( arguments, 0 ) : value;
+						if ( !( --count ) ) {
+							( any ? deferred.rejectWith : deferred.resolveWith )( promise, array );
+						}
+					};
+				},
+				i;
+			for( i = 0; i < length; i++ ) {
+				object = array[ i ];
+				if ( object && jQuery.isFunction( object.promise ) ) {
+					object.promise().then( any ? deferred.resolve : iCallback(i),
+							any ? iCallback(i) : deferred.reject );
+				} else if( any ) {
+					deferred.resolve( object );
+					break;
+				} else {
+					--count;
+				}
+			}
+			if ( !any && !count ) {
+				deferred.resolveWith( promise, array );
+			}
+		} else if ( deferred !== object ) {
+			deferred.resolve( object );
+		}
+		return promise;
+	};
+}
+
+jQuery.extend({
+
+	// Create a simple deferred (single callbacks list)
+	_Deferred: function() {
+		var // callbacks list (false when cancelled)
+			callbacks = [],
+			// stored [ context , args ]
+			fired = false,
+			// to avoid firing when already doing so
+			firing = false,
+			// the deferred itself
+			deferred  = {
+				// Add callbacks
+				done: function() {
+					if ( callbacks ) {
+						var i = 0,
+							length = arguments.length,
+							type,
+							savedFired = fired;
+						fired = false;
+						for ( ; i < length; i++ ) {
+							type = jQuery.type( arguments[ i ] );
+							if ( type === "array" ) {
+								deferred.done.apply( deferred, arguments[ i ] );
+							} else if ( type === "function" ) {
+								callbacks.push( arguments[ i ] );
+							}
+						}
+						if ( savedFired ) {
+							deferred.resolveWith( savedFired[ 0 ], savedFired[ 1 ] );
+						}
+					}
+					return this;
+				},
+				// resolve with given context and args
+				resolveWith: function( context, args ) {
+					var callback;
+					if ( callbacks && !fired && !firing ) {
+						firing = true;
+						try {
+							while( ( callback = callbacks.shift() ) ) {
+								callback.apply( context, args );
+							}
+						}
+						finally {
+							fired = [ context, args ];
+							firing = false;
+						}
+					}
+					return this;
+				},
+				// resolve with this as context (or promise if available) and given arguments
+				resolve: function() {
+					deferred.resolveWith(
+						jQuery.isFunction( this.promise ) ? this.promise() : this,
+						arguments );
+					return this;
+				},
+				// Has this deferred been resolved?
+				isResolved: function() {
+					return firing || !!fired;
+				},
+				// Cancel
+				cancel: function() {
+					callbacks = false;
+				}
+			};
+
+		return deferred;
+	},
+
+	// Full fledged deferred (two callbacks list)
+	Deferred: function( fn ) {
+		// Create the underlying deferreds
+		var defer = jQuery._Deferred(),
+			failDefer = jQuery._Deferred(),
+			i;
+		// Add missing pMethods to defer
+		defer.reject = failDefer.resolve;
+		defer.rejectWith = failDefer.resolveWith;
+		for( i in pMethods ) {
+			defer[ i ] = defer[ i ] ||
+				pMethods[ i ] && failDefer[ pMethods[i] ] ||
+				dFactories[ i ]( defer );
+		}
+		// Make sure only one callback list will be used
+		defer.done( failDefer.cancel ).fail( defer.cancel );
+		// Unexpose cancel
+		delete defer.cancel;
+		// Call given func if any
+		if ( fn ) {
+			fn.call( defer, defer );
+		}
+		return defer;
+	},
+
+	// Deferred helpers
+	when: whenFactory(),
+	whenAny: whenFactory( true ),
+	whenNone: whenFactory( true, true )
+});
+
+})( jQuery );
---
+++ test/unit/deferred.js
@@ -0,0 +1,444 @@
+module("deferred", { teardown: moduleTeardown });
+
+jQuery.each( [ "", " (new)" ], function( _, withNew ) {
+
+	function createDeferred() {
+		return withNew ? new jQuery._Deferred() : jQuery._Deferred();
+	}
+
+	test("jQuery._Deferred" + withNew, function() {
+
+		expect( 10 );
+
+		var deferred,
+			object,
+			test;
+
+		deferred = createDeferred();
+
+		test = false;
+
+		deferred.done( function( value ) {
+			equals( value , "value" , "Test pre-resolve callback" );
+			test = true;
+		} );
+
+		deferred.resolve( "value" );
+
+		ok( test , "Test pre-resolve callbacks called right away" );
+
+		test = false;
+
+		deferred.done( function( value ) {
+			equals( value , "value" , "Test post-resolve callback" );
+			test = true;
+		} );
+
+		ok( test , "Test post-resolve callbacks called right away" );
+
+		deferred.cancel();
+
+		test = true;
+
+		deferred.done( function() {
+			ok( false , "Cancel was ignored" );
+			test = false;
+		} );
+
+		ok( test , "Test cancel" );
+
+		deferred = createDeferred().resolve();
+
+		try {
+			deferred.done( function() {
+				throw "Error";
+			} , function() {
+				ok( true , "Test deferred do not cancel on exception" );
+			} );
+		} catch( e ) {
+			strictEqual( e , "Error" , "Test deferred propagates exceptions");
+			deferred.done();
+		}
+
+		test = "";
+		deferred = createDeferred().done( function() {
+
+			test += "A";
+
+		}, function() {
+
+			test += "B";
+
+		} ).resolve();
+
+		strictEqual( test , "AB" , "Test multiple done parameters" );
+
+		test = "";
+
+		deferred.done( function() {
+
+			deferred.done( function() {
+
+				test += "C";
+
+			} );
+
+			test += "A";
+
+		}, function() {
+
+			test += "B";
+		} );
+
+		strictEqual( test , "ABC" , "Test done callbacks order" );
+
+		deferred = createDeferred();
+
+		deferred.resolveWith( jQuery , [ document ] ).done( function( doc ) {
+			ok( this === jQuery && arguments.length === 1 && doc === document , "Test fire context & args" );
+		});
+	});
+} );
+
+jQuery.each( [ "", " (new)" ], function( _, withNew ) {
+
+	function createDeferred( fn ) {
+		return withNew ? new jQuery.Deferred( fn ) : jQuery.Deferred( fn );
+	}
+
+	test("jQuery.Deferred" + withNew, function() {
+
+		expect( 8 );
+
+		createDeferred().resolve().then( function() {
+			ok( true , "Success on resolve" );
+			ok( this.isResolved(), "Deferred is resolved" );
+		}, function() {
+			ok( false , "Error on resolve" );
+		}).always( function() {
+			ok( true , "Always callback on resolve" );
+		});
+
+		createDeferred().reject().then( function() {
+			ok( false , "Success on reject" );
+		}, function() {
+			ok( true , "Error on reject" );
+			ok( this.isRejected(), "Deferred is rejected" );
+		}).always( function() {
+			ok( true , "Always callback on reject" );
+		});
+
+		createDeferred( function( defer ) {
+			ok( this === defer , "Defer passed as this & first argument" );
+			this.resolve( "done" );
+		}).then( function( value ) {
+			strictEqual( value , "done" , "Passed function executed" );
+		});
+	});
+} );
+
+test("jQuery.Deferred - promise and invert", function() {
+
+	expect( 8 );
+
+	var tmp = jQuery.Deferred(),
+		tmp2 = jQuery.Deferred(),
+		promiseValue,
+		invertValue,
+		flags = {
+			promise: 0,
+			invert: 1
+		};
+
+	jQuery.each( flags, function( m1, i1 ) {
+		ok( tmp[ m1 ]() === tmp[ m1 ]() , "deferred." + m1 + "() === deferred." + m1 + "()" );
+		jQuery.each( flags, function( m2, i2 ) {
+			var m = ( i1 + i2 ) % 2 ? "invert" : "promise";
+			ok( tmp[ m ]() === tmp[ m1 ]()[ m2 ](), "deferred." + m1 + "()." + m2 + "() === deferred." + m + "()" );
+		} );
+	} );
+
+	tmp.resolve( { done: true } );
+	tmp.promise().done(function( value ) {
+		promiseValue = value;
+	});
+	tmp.invert().fail(function( value ) {
+		invertValue = value;
+	});
+
+	strictEqual( promiseValue, invertValue, "Promise and invert get the same value for done and fail respectively" );
+
+	promiseValue = invertValue = undefined;
+
+	tmp2.reject( { fail: true } );
+	tmp2.promise().fail(function( value ) {
+		promiseValue = value;
+	});
+	tmp2.invert().done(function( value ) {
+		invertValue = value;
+	});
+
+	strictEqual( promiseValue, invertValue, "Promise and invert get the same value for fail and done respectively" );
+});
+
+test( "jQuery.Deferred - chain - filtering", function() {
+
+	expect(3);
+
+	var defer = jQuery.Deferred(),
+		chained = defer.chain(function( a, b ) {
+			return a * b;
+		}),
+		value1,
+		value2,
+		value3;
+
+	chained.done(function( result ) {
+		value3 = result;
+	});
+
+	defer.done(function( a, b ) {
+		value1 = a;
+		value2 = b;
+	});
+
+	defer.resolve( 2, 3 );
+
+	strictEqual( value1, 2, "first resolve value ok" );
+	strictEqual( value2, 3, "second resolve value ok" );
+	strictEqual( value3, 6, "result of filter ok" );
+
+	jQuery.Deferred().reject().chain(function() {
+		ok( false, "chain should not be called on reject" );
+	});
+});
+
+test( "jQuery.Deferred - chain - deferred", function() {
+
+	expect(3);
+
+	var defer = jQuery.Deferred(),
+		chained = defer.chain(function( a, b ) {
+			return jQuery.Deferred(function( defer ) {
+				defer.reject( a * b );
+			});
+		}),
+		value1,
+		value2,
+		value3;
+
+	chained.fail(function( result ) {
+		value3 = result;
+	});
+
+	defer.done(function( a, b ) {
+		value1 = a;
+		value2 = b;
+	});
+
+	defer.resolve( 2, 3 );
+
+	strictEqual( value1, 2, "first resolve value ok" );
+	strictEqual( value2, 3, "second resolve value ok" );
+	strictEqual( value3, 6, "result of filter ok" );
+
+	jQuery.Deferred().reject().chain(function() {
+		ok( false, "chain should not be called on reject" );
+	});
+});
+
+jQuery.each( [ "when", "whenAny", "whenNone" ], function( _, method ) {
+	test( "jQuery." + method , function() {
+
+		expect( 23 );
+
+		// Some other objects
+		jQuery.each( {
+
+			"an empty string": "",
+			"a non-empty string": "some string",
+			"zero": 0,
+			"a number other than zero": 1,
+			"true": true,
+			"false": false,
+			"null": null,
+			"undefined": undefined,
+			"a plain object": {}
+
+		} , function( message , value ) {
+
+			ok( jQuery.isFunction( jQuery[ method ]( value ).done(function( resolveValue ) {
+				if ( method !== "whenNone" ) {
+					strictEqual( resolveValue , value , "Test the promise was resolved with " + message );
+				}
+			}).fail(function( resolveValue ) {
+				if ( method === "whenNone" ) {
+					strictEqual( resolveValue , value , "Test the promise was rejected with " + message );
+				}
+			}).promise ) , "Test " + message + " triggers the creation of a new Promise" );
+
+		} );
+
+		ok( jQuery.isFunction( jQuery[ method ]().done(function( resolveValue ) {
+			if ( method !== "whenNone" ) {
+				strictEqual( resolveValue , undefined , "Test the promise was resolved with no parameter" );
+			}
+		}).fail(function( resolveValue ) {
+			if ( method === "whenNone" ) {
+				strictEqual( resolveValue , undefined , "Test the promise was rejected with no parameter" );
+			}
+		}).promise ) , "Test calling when with no parameter triggers the creation of a new Promise" );
+
+		var cache, i;
+
+		for( i = 1 ; i < 4 ; i++ ) {
+			jQuery[ method ]( cache || jQuery.Deferred( function() {
+				this.resolve( i );
+			}) ).done(function( value ) {
+				if ( method !== "whenNone" ) {
+					strictEqual( value , 1 , "Function executed" + ( i > 1 ? " only once" : "" ) );
+					cache = value;
+				}
+			}).fail(function( value ) {
+				if ( method === "whenNone" ) {
+					strictEqual( value , 1 , "Function executed" + ( i > 1 ? " only once" : "" ) );
+					cache = value;
+				}
+			});
+		}
+	});
+} );
+
+test("jQuery.when - joined", function() {
+
+	expect(25);
+
+	var deferreds = {
+			value: 1,
+			success: jQuery.Deferred().resolve( 1 ),
+			error: jQuery.Deferred().reject( 0 ),
+			futureSuccess: jQuery.Deferred(),
+			futureError: jQuery.Deferred()
+		},
+		willSucceed = {
+			value: true,
+			success: true,
+			error: false,
+			futureSuccess: true,
+			futureError: false
+		};
+
+	jQuery.each( deferreds, function( id1, defer1 ) {
+		jQuery.each( deferreds, function( id2, defer2 ) {
+			var shouldResolve = willSucceed[ id1 ] && willSucceed[ id2 ],
+				expected = shouldResolve ? [ 1, 1 ] : [ 0, undefined ],
+				code = id1 + "/" + id2;
+			jQuery.when( defer1, defer2 ).done(function( a, b ) {
+				if ( shouldResolve ) {
+					same( [ a, b ], expected, code + " => resolve" );
+				}
+			}).fail(function( a, b ) {
+				if ( !shouldResolve ) {
+					same( [ a, b ], expected, code + " => resolve" );
+				}
+			});
+		} );
+	} );
+	deferreds.futureSuccess.resolve( 1 );
+	deferreds.futureError.reject( 0 );
+});
+
+jQuery.each( [ "whenAny", "whenNone" ], function( _, method ) {
+
+	var isWhenAny = method === "whenAny";
+
+	test("jQuery." + method + " - joined", function() {
+
+		method = jQuery[ method ];
+
+		expect(25);
+
+		var deferreds = {
+				value: 1,
+				success: jQuery.Deferred().resolve( 1 ),
+				error: jQuery.Deferred().reject( 0 ),
+				futureSuccess: jQuery.Deferred(),
+				futureError: jQuery.Deferred()
+			},
+			willSucceed = {
+				value: true,
+				success: true,
+				error: false,
+				futureSuccess: true,
+				futureError: false
+			};
+
+		jQuery.each( deferreds, function( id1, defer1 ) {
+			jQuery.each( deferreds, function( id2, defer2 ) {
+				var shouldResolve = willSucceed[ id1 ] || willSucceed[ id2 ],
+					expected = shouldResolve ? [ 1, undefined ] : [ 0, 0 ],
+					code = id1 + "/" + id2;
+				shouldResolve = shouldResolve ? isWhenAny : !isWhenAny;
+				method( defer1, defer2 ).done(function( a, b ) {
+					if ( shouldResolve ) {
+						same( [ a, b ], expected, code + " => resolve" );
+					}
+				}).fail(function( a, b ) {
+					if ( !shouldResolve ) {
+						same( [ a, b ], expected, code + " => resolve" );
+					}
+				});
+			} );
+		} );
+		deferreds.futureSuccess.resolve( 1 );
+		deferreds.futureError.reject( 0 );
+	} );
+} );
+
+jQuery.each( [ "or", "and" ], function( _, method ) {
+
+	var isAnd = method === "and";
+
+	test("jQuery.Deferred - " + method, function() {
+
+		expect(20);
+
+		var deferreds = {
+				value: 1,
+				success: jQuery.Deferred().resolve( 1 ),
+				error: jQuery.Deferred().reject( 0 ),
+				futureSuccess: jQuery.Deferred(),
+				futureError: jQuery.Deferred()
+			},
+			willSucceed = {
+				value: true,
+				success: true,
+				error: false,
+				futureSuccess: true,
+				futureError: false
+			};
+
+		jQuery.each( deferreds, function( id1, defer1 ) {
+			if ( defer1 !== 1 ) {
+				jQuery.each( deferreds, function( id2, defer2 ) {
+					var shouldResolve = isAnd ?
+							( willSucceed[ id1 ] && willSucceed[ id2 ] ) :
+							( willSucceed[ id1 ] || willSucceed[ id2 ] ),
+						expected = shouldResolve ? [ 1, isAnd ? 1 : undefined ] : [ 0, isAnd ? undefined : 0 ],
+						code = id1 + "/" + id2;
+					defer1[ method ]( defer2 ).done(function( a, b ) {
+						if ( shouldResolve ) {
+							same( [ a, b ], expected, code + " => resolve" );
+						}
+					}).fail(function( a, b ) {
+						if ( !shouldResolve ) {
+							same( [ a, b ], expected, code + " => resolve" );
+						}
+					});
+				} );
+			}
+		} );
+		deferreds.futureSuccess.resolve( 1 );
+		deferreds.futureError.reject( 0 );
+	} );
+} );
